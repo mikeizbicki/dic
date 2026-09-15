2dd35e0b834b59d83356d62cd9d5178021749594
Mike Izbicki <mike@izbicki.me>
2026-09-15 16:55:41 -0700

    update dic.sh

--- dic.sh
+++ dic.sh
@@ -3,6 +3,7 @@
 
 export DIC_MODEL=groq+qwen
 export DIC_SYSTEM="Keep your response short, between 1-20 lines. Focus on a high signal to noise ratio (audience has math/cs phd). If the question is about a computer, respond for the following system: $(uname -a)."
+[[ $- == *i* ]] && export DIC_SESSION="$$"
 
 alias qwen='dic -m groq+qwen'
 alias fable='dic -m anthropic+fable'
